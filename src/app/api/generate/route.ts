f1d2b49fc300eb475463bdbbc2d6cb4fc30a48e4
neo@zillinks.com <keunhey0107@gmail.com>
2023-12-28 02:33:35 +0900

    Update: max duration

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -1,10 +1,10 @@
 import { NextResponse, NextRequest } from 'next/server';
 import OpenAI from 'openai';
 
-export const config = {
-  runtime: 'edge',
-};
-export const maxDuration = 5;
+// export const config = {
+//   runtime: 'edge',
+// };
+// export const maxDuration = 5;
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
