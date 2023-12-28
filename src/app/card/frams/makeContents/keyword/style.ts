d9571773c84dee1df769a23c00df971fb552d6e1
neo@zillinks.com <keunhey0107@gmail.com>
2023-12-28 09:44:05 +0900

    Upda간격 스타일 수정

--- src/app/card/frams/makeContents/keyword/style.ts
+++ src/app/card/frams/makeContents/keyword/style.ts
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 import mediaQuery from '@components/lib/styles/theme/mediaQuery';
 
 export const Wrap = styled.div`
-  padding: 102px 0 116px;
+  padding: 102px 0 117px;
 
   ${mediaQuery.tablet} {
     padding: 42px 0 52px;
--- src/app/card/frams/makeContents/letter/style.ts
+++ src/app/card/frams/makeContents/letter/style.ts
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 import mediaQuery from '@components/lib/styles/theme/mediaQuery';
 
 export const WrapLetter = styled.div`
-  padding: 119px 0 117px;
+  padding: 94px 0 113px;
 
   ${mediaQuery.tablet} {
     padding: 45px 0 47px;
