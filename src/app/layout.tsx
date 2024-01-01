7bdb48f57bc451d382bb7166fcff68fec1e80591
neo@zillinks.com <keunhey0107@gmail.com>
2024-01-01 15:10:18 +0900

    Update: pwa 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@ import Footer from '@components/components/footer';
 import EmotionProvider from '@components/lib/provider/emotion';
 import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next';
-import Head from 'next/head';
 
 export const metadata: Metadata = {
+  manifest: '/manifest.json',
   title: 'AI 신년카드 만들기',
   description: '나만의 신년카드 만들기',
 };
