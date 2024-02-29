368078bb399fec751bb5e28ba23cd18806d431d4
neo@zillinks.com <keunhey0107@gmail.com>
2024-02-29 14:11:00 +0900

    Delete: 콘솔 삭제

--- src/components/share/controller/index.tsx
+++ src/components/share/controller/index.tsx
@@ -26,7 +26,6 @@ const Controller = ({
         .then(() => console.log("Share was successful."))
         .catch((error) => console.log("Sharing failed", error));
     } else {
-      console.log(`Your system doesn't support sharing files.`);
       onNonNativeShare?.();
     }
   };
